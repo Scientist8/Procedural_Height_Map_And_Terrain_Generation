bfe45e5b3e52d84f81b88a02f081d3df6d34bd50
Can Karaca <cankaraca94@gmail.com>
2022-03-17 18:20:33 +0300

    Fall off generator fix

--- HeightMapAndTerrainGeneration/Assets/GameFolders/Scripts/HeightMapGenerator.cs
+++ HeightMapAndTerrainGeneration/Assets/GameFolders/Scripts/HeightMapGenerator.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public static class HeightMapGenerator
 {
 
-    public static HeightMap GenerateHeightMap(int width, int height, HeightMapSettings settings, Vector2 sampleCentre)
-    {
-        float[,] values = Noise.GenerateNoiseMap(width, height, settings.noiseSettings, sampleCentre);
+	static float[,] falloffMap;
+
+	public static HeightMap GenerateHeightMap(int width, int height, HeightMapSettings settings, Vector2 sampleCentre)
+	{
+		float[,] values = Noise.GenerateNoiseMap(width, height, settings.noiseSettings, sampleCentre);
 
 		//Animation curve sometimes gives wrong values when threaded, so we make a copy of it
 		AnimationCurve heightCurve_threadsafe = new AnimationCurve(settings.heightCurve.keys);
-
 		float minValue = float.MaxValue;
 		float maxValue = float.MinValue;
+
+		if (settings.useFalloff)
+		{
+			if (falloffMap == null)
+			{
+				falloffMap = FallOffGenerator.GenerateFallOffMap(width);
+			}
+		}
 
 		for (int i = 0; i < width; i++)
 		{
 			for (int j = 0; j < height; j++)
 			{
-				values[i, j] *= heightCurve_threadsafe.Evaluate(values[i, j]) * settings.heightMultiplier;
+				values[i, j] *= heightCurve_threadsafe.Evaluate(values[i, j] - (settings.useFalloff ? falloffMap[i, j] : 0)) * settings.heightMultiplier;
+
 
 				if (values[i, j] > maxValue)
 				{
